ec7c62830fe1e89cd3de4b4a3127dbf3ea72e73e
Denis Pitsul <denia.pitsul@gmail.com>
2023-11-14 09:57:14 +0200

    header auth content

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,7 +8,7 @@ import { AppContext } from "../../context/Context";
 import MobileMenu from "../mobile-menu/MobileMenu";
 
 const Header: FunctionComponent = () => {
-    const {setIsAuth, userName, setUserName, basketItems} = useContext(AppContext);
+    const {isAuth, setIsAuth, userName, setUserName, basketItems} = useContext(AppContext);
     const [count, setCount] = useState<number>(0);
     const [isMobileMenuActive, setIsMobileMenuActive] = useState<boolean>(false);
     const router = useNavigate();
@@ -37,35 +37,42 @@ const Header: FunctionComponent = () => {
                 <div className="container">
                     <div className="header-container">
                         <Link className="header-home-link" to='/'>X-course task / Denis Pitsul</Link>
-                        <div className="header-content">
-                            <button 
-                                type="button" 
-                                className="header-burger-btn"
-                                onClick={onBurgerBtnClick}>
-                                <svg className="header-burger-icon">
-                                    <use href={icons + "#icon-burger"}></use>
-                                </svg>
-                            </button>
-                            <ul className="header-content-list">
-                                <li className="header-content-item">
-                                    <Link className="header-cart-link" to='/cart'>
-                                        <img className="header-cart-img" src={cardImg} alt="cart"/>
-                                        <span className={count === 0 ? "header-cart-count-hidden" : "header-cart-count"}>{count}</span>
-                                    </Link>
-                                </li>
-                                <li className="header-content-item">
-                                    <button onClick={onSignOutClick} type="button" className="header-sing-btn">Sing-Out</button>
-                                </li>
-                                <li className="header-content-item">
-                                    <Link className="header-sing-link" to='/sign-in'>
-                                        <span className="header-avatar-span">
-                                            <img className="header-avatar-img" src={avatarImg} alt="avatar"/>
-                                        </span>
-                                        <span className="header-username">{userName === '' ? 'Username' : userName}</span>
-                                    </Link>
-                                </li>
-                            </ul>                            
-                        </div>
+
+                        {
+                        isAuth 
+                            ? 
+                            <div className="header-content">
+                                <button 
+                                    type="button" 
+                                    className="header-burger-btn"
+                                    onClick={onBurgerBtnClick}>
+                                    <svg className="header-burger-icon">
+                                        <use href={icons + "#icon-burger"}></use>
+                                    </svg>
+                                </button>
+                                <ul className="header-content-list">
+                                    <li className="header-content-item">
+                                        <Link className="header-cart-link" to='/cart'>
+                                            <img className="header-cart-img" src={cardImg} alt="cart"/>
+                                            <span className={count === 0 ? "header-cart-count-hidden" : "header-cart-count"}>{count}</span>
+                                        </Link>
+                                    </li>
+                                    <li className="header-content-item">
+                                        <button onClick={onSignOutClick} type="button" className="header-sing-btn">Sing-Out</button>
+                                    </li>
+                                    <li className="header-content-item">
+                                        <Link className="header-sing-link" to='/sign-in'>
+                                            <span className="header-avatar-span">
+                                                <img className="header-avatar-img" src={avatarImg} alt="avatar"/>
+                                            </span>
+                                            <span className="header-username">{userName === '' ? 'Username' : userName}</span>
+                                        </Link>
+                                    </li>
+                                </ul>                            
+                            </div>
+                            :
+                            <div></div>
+                        }
                     </div>
                 </div>
             </nav>
